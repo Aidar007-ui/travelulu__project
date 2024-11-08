42a023a55eb3927f0dcafeb369ff5628a10783df
unknown <cholponbaevaidar8@gmail.com>
2024-11-08 17:43:10 +0300

    add media

--- js.js
+++ js.js
@@ -3,3 +3,10 @@ let box = document.querySelector('.popup')
 let showPopup = () =>{
     box.classList.toggle('active')
 }
+
+let menu = document.querySelector('.burger__box')
+let plans = document.querySelector('.plans__bottom')
+let showBurger__box = () => {
+    menu.classList.toggle('open')
+    plans.classList.toggle('open')
+}
